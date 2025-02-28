04d96083bcd3a02de83582b7b76831c752c2b5c7
Rajesh Goyal <rajesh.goyal@qoala.id>
2025-02-28 18:47:58 +0530

    a new app version push

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask running in Kubernetes! - V2"
+    return "Hello from Flask running in Kubernetes! - V2.0.0"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
